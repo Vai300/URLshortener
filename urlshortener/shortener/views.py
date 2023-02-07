21eee5bbf0155f4bca41de7c2a41470cd4c5bde0
Vai300 <113701569+Vai300@users.noreply.github.com>
2023-02-08 01:34:06 +0530

    Add files via upload

---
+++ urlshortener/shortener/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+import uuid
+from .models import Url
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        if ("http://www" not in link) and ("https://www" not in link) :
+            link = "http://www." + link + ".com"
+        uid = str(uuid.uuid4())[:3] #give 3 letters for uid
+        
+        new_url = Url(link=link,uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+def go(request, pk):
+    url_details= Url.objects.get(uuid=pk)
+    print(url_details.link)
+    return redirect(url_details.link)      
+
+
+
+
+
+
+
+
+
